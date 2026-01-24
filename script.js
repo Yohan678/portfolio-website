5876ab257cd827b61e293c938cb038ce541ddec5
Yohan Yoon <beginnerdevyohan@gmail.com>
2026-01-24 01:50:38 -0500

    Fix script conflict

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -12,19 +11,4 @@ fetch('nav.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
-=======
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        })
-    }) 
-})
-
-fetch('nav.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar-placeholder').innerHTML = data;
->>>>>>> f8001121ee3cf74cbc0be2dbef2d7343cf52eac5
     });
